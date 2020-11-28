f557f33b143e9200dd24e617cc77ad2a833f242b
Nikhil Naik <naiknikhil925@gmail.com>
2020-11-28 23:12:03 +0530

    file updated

---
+++ index.js
@@ -0,0 +1,61 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 1500,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('.home__title',{}); 
+sr.reveal('.button',{delay: 100}); 
+sr.reveal('.home__img',{delay: 300}); 
+sr.reveal('.home__social-icon',{ interval: 100}); 
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img',{}); 
+sr.reveal('.about__subtitle',{delay: 300}); 
+sr.reveal('.about__text',{delay: 300}); 
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle',{}); 
+sr.reveal('.skills__text',{}); 
+sr.reveal('.skills__data',{interval: 100}); 
+sr.reveal('.skills__img',{delay: 500});
+
+/*SCROLL WORK*/
+sr.reveal('.work__img',{interval: 200}); 
+
+
+
+/* SCROLL PROJECT */
+sr.reveal('.list-non-bullet',{interval: 500}); 
+sr.reveal('.margin-md',{delay: 100});
+sr.reveal('.ow',{delay: 200});
